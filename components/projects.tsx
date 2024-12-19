fe4d28af1fa668fe1b578057ece07e0a9c057890
thiagotfsilva <thiagophsilva@hotmail.com>
2024-12-19 17:01:47 -0300

    implementando projects section

---
+++ components/projects.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+import { projectsData } from '@/lib/data'
+import Project from './project'
+
+const Projects = () => {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div className="">
+        {
+          projectsData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project} />
+            </React.Fragment>
+          ))
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Projects
