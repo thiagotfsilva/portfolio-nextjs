5714ba1ac96466accc743be1719ee05a0ee04935
thiagotfsilva <thiagophsilva@hotmail.com>
2024-12-26 00:26:01 -0300

    implements darkmode

--- components/contact.tsx
+++ components/contact.tsx
@@ -29,12 +29,12 @@ const Contact = () => {
      }}
     >
       <SectionHeading>Contact me</SectionHeading>
-      <p className='text-gray-700 -mt-6'>
+      <p className='text-gray-700 -mt-6 dark:text-white/80'>
         Please contact me directly at <a className='underline' href='mailto:example@gmail.com'>example@gmail.com</a>or through this form.
       </p>
 
       <form 
-       className='mt-10 flex flex-col' 
+       className='mt-10 flex flex-col dark:text-black' 
        action={async (formData) => {
         const { data, error } = await sendEmail(formData);
 
@@ -48,7 +48,7 @@ const Contact = () => {
       >
         <input 
          id='email'
-         className='h-14 px-4 rounded-lg borderBlack'
+         className='h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
          name='email'
          required 
          maxLength={500} 
@@ -58,7 +58,7 @@ const Contact = () => {
         <textarea 
          id='message'
          name='message'
-         className='h-52 my-3 rounded-lg borderBlack p-4'
+         className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
          placeholder='Your message'
          required 
          maxLength={5000}
--- components/experience.tsx
+++ components/experience.tsx
@@ -26,18 +26,22 @@ const Experience = () => {
 								padding: "1.3rem 2rem"
 							 }}
 							 contentArrowStyle={{
-								borderRight: "0.4rem solid #9ca3af",
+								borderRight: theme === "light"
+								 ? "0.4rem solid #9ca3af"
+								 : "0.4rem solid rgba(255, 255, .5)",
 							 }}
 							 date={item.date}
 							 icon={item.icon}
 							 iconStyle={{
-								background: "white",
+								background: theme === "light" ? "white" : "rgba(255, 255, 255, .05",
 								fontSize: "1.5rem"
 							 }}
 							>
 								<h3 className='font-semibold capitalize'>{item.title}</h3>
 								<p className='font-normal !mt-0'>{item.location}</p>
-								<p>{item.description}</p>
+								<p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
+									{item.description}
+								</p>
 							</VerticalTimelineElement>
 						</React.Fragment>
 					))
